102baabb330f9a796db9b3e3356e33c8a337ad45
Sweeti2004 <sweeti754940@gmail.com>
2025-06-15 09:46:32 +0530

    Add main layout structure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 
 import './App.css';
-import { Button } from 'react-bootstrap';
 import Entry from './page/entry/Entry.page';
+import DefaultLayout from './layout/DefaultLayout';
 function App() {
   return (
     <div className="App">
       
-      <Entry/>
+      {/* <Entry/> */}
+      <DefaultLayout>
+        send page comp
+      </DefaultLayout>
+
       
     </div>
   );
