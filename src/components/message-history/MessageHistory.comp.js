7db463858395d2f943dc659d02f829ef83c672ea
Sweeti2004 <sweeti754940@gmail.com>
2025-06-17 02:50:38 +0530

    ticket landing page added

---
+++ src/components/message-history/MessageHistory.comp.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './message-history.css'
+const MessageHistory = ({msg}) => {
+    if(!msg ) return null
+  return msg.map((row, i) => (
+    <div key={i} className="message-history mt-3">
+      <div className="send font-weight-bold text-secondary">
+        <div className="sender">{row.messageBy}</div>
+        <div className="date">
+         {/* {row.msgAt && new Date(row.msgAt).toLocaleString()} */}
+         {row.date}
+        </div>
+      </div>
+      <div className="message">{row.message}</div>
+    </div>
+  ));
+}
+
+export default MessageHistory
