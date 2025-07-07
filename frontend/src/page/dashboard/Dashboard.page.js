07471c36001201161cb01ac41b26dac71209dd72
Sweeti2004 <sweeti754940@gmail.com>
2025-07-07 14:24:38 +0530

    dashboard data fetched and updated

--- frontend/src/page/dashboard/Dashboard.page.js
+++ frontend/src/page/dashboard/Dashboard.page.js
@@ -1,10 +1,21 @@
-import React from 'react'
+import React,{useEffect} from 'react'
+import { useSelector,useDispatch } from 'react-redux';
 import { Container, Row, Col, Button, Breadcrumb } from "react-bootstrap";
 import TicketTable from '../../components/ticket-table/TicketTable.comp';
 import tickets from '../../assets/data/dummy-tickets.json'
 import PageBreadcrumb from '../../components/breadcrumb/Breadcrumb.comp';
 import {Link} from 'react-router-dom'
+import {fetchAllTickets} from "../ticket-list/ticketsAction"
 const Dashboard = () => {
+  const {tickets}=useSelector(state=>state.tickets)
+  const dispatch=useDispatch()
+  useEffect(()=>{
+    if(!tickets.length){
+      dispatch(fetchAllTickets())
+    }
+  },[tickets,dispatch])
+  const pendingTickets=tickets.filter(row=>row.status !=="Closed")
+  const totalTickets=tickets.length;
   return (
    <Container>
     <Row>
@@ -27,8 +38,8 @@ const Dashboard = () => {
       <Row>
         <Col className="text-center mt-5 mb-2">
           
-            <div>Total Tickets:50</div>
-            <div>Pending Tickets:5</div>
+            <div>Total Tickets:{totalTickets}</div>
+            <div>Pending Tickets:{pendingTickets.length}</div>
         </Col>
       </Row>
       <Row>
